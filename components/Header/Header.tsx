5bfbc262f1a6bbbfe0296b97b8f1608d8089ae9a
Iryna Ponikarchyk <irishka.ponikarchik@ukr.net>
2025-11-25 23:26:11 +0200

    add fix header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import css from "./Header.module.css"
+
+export default function Header() {
+  return (
+    <>
+      <header className={css.header}>
+          <div className={`container ${css.headerContainer}`}>
+            <svg className={css.logoIcon} width={104} height={16}>
+              <use href="/icons/logo.svg"/>
+            </svg>
+            <nav className={css.navigation}>
+              <Link className={css.headerLink} href="/">Home</Link>
+              <Link className={css.headerLink} href="/catalog">Catalog</Link>
+            </nav>
+          </div>
+    </header>
+    </>
+  )
+}
